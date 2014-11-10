476e0eb27985f8e74d84d8c5cb441cb9299a1dd5
Ben-Ho <benjamin.hohenwarter@gmail.com>
2014-11-10 20:52:27 +0100

    Expressions fuer ausgelagerte Adressen

--- models/Congregations.php
+++ models/Congregations.php
@@ -9,7 +9,7 @@ class Congregations extends Kwf_Model_Db
             'refModelClass' => 'Circles',
             'column' => 'circle_id'
         ),
-        'Adress' => array(
+        'Address' => array(
             'refModelClass' => 'CongregationAddresses',
             'column' => 'address_id'
         )
@@ -18,4 +18,12 @@ class Congregations extends Kwf_Model_Db
     protected $_dependentModels = array(
         'Speakers' => 'Speakers'
     );
+    protected function _init()
+    {
+        parent::_init();
+        $this->_exprs['street'] = new Kwf_Model_Select_Expr_Parent('Address', 'street');
+        $this->_exprs['zip'] = new Kwf_Model_Select_Expr_Parent('Address', 'zip');
+        $this->_exprs['city'] = new Kwf_Model_Select_Expr_Parent('Address', 'city');
+        $this->_exprs['country'] = new Kwf_Model_Select_Expr_Parent('Address', 'country');
+    }
 }
