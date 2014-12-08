46a064f37d0ea2ed4c276ca3ced68e7a08ba65c3
Ben-Ho <benjamin.hohenwarter@gmail.com>
2014-12-08 14:58:18 +0100

    Hinzufuegen von Trl-Version von Title fuer Menu

---
+++ components/Title/Trl/Component.php
@@ -0,0 +1,17 @@
+<?php
+class Title_Trl_Component extends Kwc_Abstract_Composite_Trl_Component
+{
+    public static function getSettings($masterComponentClass)
+    {
+        $ret = parent::getSettings($masterComponentClass);
+        $ret['plugins'] = array('Login_Plugin_Component');
+        return $ret;
+    }
+
+    public function getTemplateVars(Kwf_Component_Renderer_Abstract $renderer = null)
+    {
+        $ret = parent::getTemplateVars($renderer);
+        $ret['pageName'] = $this->getData()->getPage()->getRow()->name;
+        return $ret;
+    }
+}
