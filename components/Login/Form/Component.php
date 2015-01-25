02b0adeafcc0ce52c0802e0d557956f225fe91df
Ben-Ho <benjamin.hohenwarter@gmail.com>
2015-01-25 20:00:52 +0100

    Moeglichkeit mit Vers oder Name einzuloggen

--- components/Login/Form/Component.php
+++ components/Login/Form/Component.php
@@ -11,35 +11,34 @@ class Login_Form_Component extends Kwc_User_Login_Form_Component
 
     protected function _afterSave(Kwf_Model_Row_Interface $row)
     {
-        $tryLogin = false;
         if (strpos($row->text, '@') !== false) {
-            $tryLogin = true;
             $row->email = $row->text;
+            return parent::_afterSave($row);
+        }
+
+        $userSelect = new Kwf_Model_Select();
+        $select = new Kwf_Model_Select();
+        $select->whereEquals('name', $row->text);
+        $congregationRow = Kwf_Model_Abstract::getInstance('Congregations')->getRow($select);
+        if ($congregationRow) {
+            $userSelect->whereEquals('congregation_id', $congregationRow->id);
         } else {
-            $splited = explode(' ', $row->text);
-            if (count($splited) == 2) {
-                //Firstname and Lastname will
-                $firstname = $splited[0];
-                $lastname = $splited[1];
-                $select = new Kwf_Model_Select();
-                $select->whereEquals('firstname', $firstname);
-                $select->whereEquals('lastname', $lastname);
-                $rows = Kwf_Registry::get('userModel')->getRows($select);
-                foreach ($rows as $user) {
-                    $result = $this->_getAuthenticateResult($user->email, $row->password);
-                    if ($result->isValid()) {
-                        $row->email = $user->email;
-                        $tryLogin = true;
-                        break;
-                    }
-                }
+            $userSelect->whereEquals('name', $row->text);
+        }
+
+        $rows = Kwf_Registry::get('userModel')->getRows($userSelect);
+        if (count($rows) > 1) {
+            $this->_errors[] = array('message' => $this->getData()->trl('Diesem Benutzernamen sind mehrere Benutzer zugeordnet. Bitte melde dich mit deiner E-Mail-Adresse ein.'));
+            return;
+        }
+        foreach ($rows as $user) {
+            $result = $this->_getAuthenticateResult($user->email, $row->password);
+            if ($result->isValid()) {
+                $row->email = $user->email;
+                break;
             }
         }
-        if ($tryLogin) {
-            parent::_afterSave($row);
-        } else {
-            $this->_errors[] = array('message' => $this->getData()->trl('Bitte geben Sie Ihre korrekten Zugangsdaten ein!'));
-        }
+        parent::_afterSave($row);
     }
 
     private function _getAuthenticateResult($identity, $credential)
