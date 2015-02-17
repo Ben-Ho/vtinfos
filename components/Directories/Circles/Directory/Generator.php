f9ed397af6901ea853ac57813a5cdfc4a75bc3d2
Ben-Ho <benjamin.hohenwarter@gmail.com>
2015-02-17 21:12:54 +0100

    Benoetigte Generator funktionen implementiert

--- components/Directories/Circles/Directory/Generator.php
+++ components/Directories/Circles/Directory/Generator.php
@@ -4,7 +4,15 @@ class Directories_Circles_Directory_Generator extends Kwf_Component_Generator_Ta
     protected function _formatSelect($parentData, $select)
     {
         $select = parent::_formatSelect($parentData, $select);
-        $select->whereEquals('group_id', $parentData->parent->row->id);
+        if ($parentData)
+            $select->whereEquals('group_id', $parentData->parent->row->id);
         return $select;
+    }
+
+    protected function _getParentDataByRow($row, $select)
+    {
+        $component = Kwf_Component_Data_Root::getInstance()
+            ->getComponentByClass('Directories_CircleGroups_Directory_Component');
+        return $component->getChildComponent('-'.$row->group_id);
     }
 }
