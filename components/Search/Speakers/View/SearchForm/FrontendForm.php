16114d23719f194e07a6e9033c722310e9eae460
Ben-Ho <benjamin.hohenwarter@gmail.com>
2015-02-28 18:19:49 +0100

    Numberfield anstelle von textfield, damit weniger fehler

--- components/Search/Speakers/View/SearchForm/FrontendForm.php
+++ components/Search/Speakers/View/SearchForm/FrontendForm.php
@@ -30,7 +30,7 @@ class Search_Speakers_View_SearchForm_FrontendForm extends Kwf_Form
         $comboBox->setShowNoSelection(true);
         $comboBox->setValues($data);
         $this->add($comboBox);
-        $this->add(new Kwf_Form_Field_TextField('distance', trlStatic('Luftlinie (km)')));
+        $this->add(new Kwf_Form_Field_NumberField('distance', trlStatic('Luftlinie (km)')));
         $this->add(new Kwf_Form_Field_Checkbox('no_beard', trlStatic('Kein Voll-/Modebart')));
     }
 }
