d4fb84a2690035c70e30f0a117058a7f6d52642a
Ben-Ho <benjamin.hohenwarter@gmail.com>
2015-03-05 07:45:22 +0100

    Hinzufuegen von Eingabefeld fuer benutzername

--- app/UserForm.php
+++ app/UserForm.php
@@ -4,6 +4,7 @@ class UserForm extends Kwf_User_Form
     protected function _initFields()
     {
         parent::_initFields();
+        $this->fields->add(new Kwf_Form_Field_TextField('wp_user', trl('Benutzername')));
         $congregationRows = Kwf_Model_Abstract::getInstance('Congregations')->getRows();
         $this->fields->add(new Kwf_Form_Field_Select('congregation_id', trl('Versammlung')))
             ->setValues($congregationRows);
