8e34e0305c1e61b28c396007f7961b6ef4fa8789
Ben-Ho <benjamin.hohenwarter@gmail.com>
2015-03-05 09:22:28 +0100

    Reihenfolge v in user-form

--- app/UserForm.php
+++ app/UserForm.php
@@ -5,7 +5,9 @@ class UserForm extends Kwf_User_Form
     {
         parent::_initFields();
         $this->fields->add(new Kwf_Form_Field_TextField('wp_user', trl('Benutzername')));
-        $congregationRows = Kwf_Model_Abstract::getInstance('Congregations')->getRows();
+        $select = new Kwf_Model_Select();
+        $select->order('name');
+        $congregationRows = Kwf_Model_Abstract::getInstance('Congregations')->getRows($select);
         $this->fields->add(new Kwf_Form_Field_Select('congregation_id', trl('Versammlung')))
             ->setValues($congregationRows);
     }
