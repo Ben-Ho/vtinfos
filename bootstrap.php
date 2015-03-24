af27d3dd0108a8d46ffff3d66a0e56f3311e8e93
Ben-Ho <benjamin.hohenwarter@gmail.com>
2015-03-24 20:22:14 +0100

    Deaktiviere sitemap.xml und anpassen von robots.txt

--- bootstrap.php
+++ bootstrap.php
@@ -2,7 +2,7 @@
 chdir(dirname(__FILE__));
 require_once 'kwf-lib/Kwf/Setup.php';
 Kwf_Setup::setUp();
-Kwf_Setup::dispatchKwc();
+Setup::dispatchKwc();
 Kwf_Setup::dispatchMedia();
 
 $front = Kwf_Controller_Front_Component::getInstance();
