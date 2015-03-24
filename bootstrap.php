4c125283a2bc661cf1f3d256011fd97cd6906cfa
Ben-Ho <benjamin.hohenwarter@gmail.com>
2015-03-24 21:16:28 +0100

    Loesche auto-login cookies

--- bootstrap.php
+++ bootstrap.php
@@ -2,6 +2,10 @@
 chdir(dirname(__FILE__));
 require_once 'kwf-lib/Kwf/Setup.php';
 Kwf_Setup::setUp();
+
+setcookie('feAutologin', '', time() - 3600, '/', null, Kwf_Util_Https::supportsHttps(), true);
+setcookie('hasFeAutologin', '', time() - 3600, '/', null, false, true);
+
 Setup::dispatchKwc();
 Kwf_Setup::dispatchMedia();
 
