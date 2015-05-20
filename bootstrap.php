460970d7c90263d6286a9db7037583c81f242e4a
Ben-Ho <benjamin.hohenwarter@gmail.com>
2015-05-20 20:22:23 +0200

    Hinzufuegen von manifest.json

--- bootstrap.php
+++ bootstrap.php
@@ -2,7 +2,10 @@
 chdir(dirname(__FILE__));
 require_once 'kwf-lib/Kwf/Setup.php';
 Kwf_Setup::setUp();
-
+if (isset($_SERVER['REQUEST_URI']) && $_SERVER['REQUEST_URI'] == '/manifest.json') {
+    echo file_get_contents('./app/manifest.json');
+    exit;
+}
 setcookie('feAutologin', '', time() - 3600, '/', null, Kwf_Util_Https::supportsHttps(), true);
 setcookie('hasFeAutologin', '', time() - 3600, '/', null, false, true);
 
