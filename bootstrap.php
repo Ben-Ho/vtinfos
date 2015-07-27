aebf6fa90501544eb77dbf0b8b7249390177761f
Ben-Ho <benjamin.hohenwarter@gmail.com>
2015-07-27 17:36:01 +0200

    Entferne cookie-entfern-code :-)

--- bootstrap.php
+++ bootstrap.php
@@ -6,8 +6,6 @@ if (isset($_SERVER['REQUEST_URI']) && $_SERVER['REQUEST_URI'] == '/manifest.json
     echo file_get_contents('./app/manifest.json');
     exit;
 }
-setcookie('feAutologin', '', time() - 3600, '/', null, Kwf_Util_Https::supportsHttps(), true);
-setcookie('hasFeAutologin', '', time() - 3600, '/', null, false, true);
 
 Setup::dispatchKwc();
 Kwf_Setup::dispatchMedia();
