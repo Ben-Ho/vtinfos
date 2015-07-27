98fde0711e1e31ee7be7a0a91e092a7734e8237a
Ben-Ho <benjamin.hohenwarter@gmail.com>
2015-07-27 18:18:46 +0200

    Abfangen von Microsoft Office Protocol Discovery, muellt sonst log zu

--- bootstrap.php
+++ bootstrap.php
@@ -6,6 +6,12 @@ if (isset($_SERVER['REQUEST_URI']) && $_SERVER['REQUEST_URI'] == '/manifest.json
     echo file_get_contents('./app/manifest.json');
     exit;
 }
+if (isset($_SERVER['HTTP_USER_AGENT'])
+    && $_SERVER['HTTP_USER_AGENT'] == 'Microsoft Office Protocol Discovery'
+) {
+    echo '';
+    exit;
+}
 
 Setup::dispatchKwc();
 Kwf_Setup::dispatchMedia();
