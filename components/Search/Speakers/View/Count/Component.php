a1d7cd8dcf9cd59f2616f27744eebf0eea34c20a
josef <benjamin.hohenwarter@gmail.com>
2016-02-06 23:38:47 +0100

    Verwenden und stylen von count-component fuer suche
    So koennen gewisse statistische informationen ueber
    die Daten ausgegeben werden.

---
+++ components/Search/Speakers/View/Count/Component.php
@@ -0,0 +1,11 @@
+<?php
+class Search_Speakers_View_Count_Component extends Kwc_Directories_List_View_Count_Component
+{
+    public static function getSettings()
+    {
+        $ret = parent::getSettings();
+        $ret['placeholder']['total'] = trlStatic('Anzahl Suchergebnisse').': ';
+        return $ret;
+    }
+}
+
