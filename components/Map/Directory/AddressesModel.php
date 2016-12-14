31ce290f8732cdc89e08ee159cfd0006d75c57e7
Benjamin Hohenwarter <bh@vivid-planet.com>
2016-12-14 12:12:24 +0100

    Update auf 4.2

--- components/Map/Directory/AddressesModel.php
+++ components/Map/Directory/AddressesModel.php
@@ -8,24 +8,28 @@ class Map_Directory_AddressesModel extends Kwf_Model_FnF
             'coordinator', 'talk_organiser', 'circle_id', 'street', 'zip', 'country',
             'longitude', 'latitude', 'note', 'congregationIds'
         );
-        $select = new Kwf_Model_Select();
-        $select->where(new Kwf_Model_Select_Expr_NotEquals('street', ''));
-        $select->where(new Kwf_Model_Select_Expr_NotEquals('zip', ''));
-        $select->where(new Kwf_Model_Select_Expr_NotEquals('city', ''));
-//         $select->where(new Kwf_Model_Select_Expr_NotEquals('country', ''));
-        $rows = Kwf_Model_Abstract::getInstance('Congregations')->export(
-            Kwf_Model_Abstract::FORMAT_ARRAY,
-            $select
-        );
-        $uniqueRows = array();
-        foreach ($rows as $row) {
-            if (!isset($uniqueRows[$row['latitude'].$row['longitude']])) {
-                $row['congregationIds'] = '';
-                $uniqueRows[$row['latitude'].$row['longitude']] = $row;
+        if (Kwf_Registry::get('db')) {
+            // Nur daten laden wenn datenbank da ist.
+            // Kwf_Model_Provider_Components instanziert nämlich alle Models beim builden der Events, ohne Datenbank
+            $select = new Kwf_Model_Select();
+            $select->where(new Kwf_Model_Select_Expr_NotEquals('street', ''));
+            $select->where(new Kwf_Model_Select_Expr_NotEquals('zip', ''));
+            $select->where(new Kwf_Model_Select_Expr_NotEquals('city', ''));
+//             $select->where(new Kwf_Model_Select_Expr_NotEquals('country', ''));
+            $rows = Kwf_Model_Abstract::getInstance('Congregations')->export(
+                Kwf_Model_Abstract::FORMAT_ARRAY,
+                $select
+            );
+            $uniqueRows = array();
+            foreach ($rows as $row) {
+                if (!isset($uniqueRows[$row['latitude'].$row['longitude']])) {
+                    $row['congregationIds'] = '';
+                    $uniqueRows[$row['latitude'].$row['longitude']] = $row;
+                }
+                $uniqueRows[$row['latitude'].$row['longitude']]['congregationIds'] .= $row['id'].';';
             }
-            $uniqueRows[$row['latitude'].$row['longitude']]['congregationIds'] .= $row['id'].';';
+            $config['data'] = array_values($uniqueRows);
         }
-        $config['data'] = array_values($uniqueRows);
         parent::__construct($config);
     }
 }
