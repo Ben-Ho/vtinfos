55e770d751a71db5ebf78ffc43a18cb8c8923608
Benjamin Hohenwarter <bh@vivid-planet.com>
2017-03-22 12:23:31 +0100

    Styling

--- components/Search/Speakers/View/Component.js
+++
@@ -1,2 +0,0 @@
-var responsiveEl = require('kwf/responsive-el');
-responsiveEl('.kwcClass', [400, 800]);
--- components/Search/Speakers/View/SearchForm/FrontendForm.php
+++ components/Search/Speakers/View/SearchForm/FrontendForm.php
@@ -37,9 +37,7 @@ class Search_Speakers_View_SearchForm_FrontendForm extends Kwf_Form
         $comboBox->setShowNoSelection(true);
         $comboBox->setValues($data);
         $this->add($comboBox);
-        $this->add(new Kwf_Form_Field_TextArea('distance', trlStatic('Luftlinie (km)')))
-            ->setHeight(18)
-            ->setWidth(219);
+        $this->add(new Kwf_Form_Field_TextField('distance', trlStatic('Luftlinie (km)')));
         $this->add(new Kwf_Form_Field_Checkbox('no_beard', trlStatic('Kein Voll-/Modebart')));
     }
 }
