f548db3c408ed01e7097e890ab8a7f129ec491b5
Benjamin Hohenwarter <bh@vivid-planet.com>
2017-07-17 11:55:58 +0200

    Hauptmenue: firstchild nicht klickbar, anpassen von styling dafuer

--- components/Menu/Main/Component.php
+++ components/Menu/Main/Component.php
@@ -9,4 +9,13 @@ class Menu_Main_Component extends Kwc_Menu_Expanded_Component
         $ret['plugins'] = array('Login_Plugin_Component');
         return $ret;
     }
+
+    public function getTemplateVars(Kwf_Component_Renderer_Abstract $renderer)
+    {
+        $ret = parent::getTemplateVars($renderer);
+        foreach ($ret['menu'] as $k=>$m) {
+            $ret['menu'][$k]['isFirstChildPage'] = is_instance_of($m['data'], 'Kwc_Basic_LinkTag_FirstChildPage_Data');
+        }
+        return $ret;
+    }
 }
